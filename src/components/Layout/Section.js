d8b76884cd37a089334b58b5d3d561390366eff4
Vpsnak <info.vpsnak@gmail.com>
2019-03-26 23:39:58 +0200

    * Observer for scroll

--- src/components/Layout/Section.js
+++ src/components/Layout/Section.js
@@ -1,67 +1,63 @@
 import React, {Component} from "react"
 import PropTypes from "prop-types"
+import {InView} from 'react-intersection-observer'
 
 import {Container, Row, Col} from 'reactstrap';
 
 class Section extends Component {
     constructor(props) {
         super(props);
-
         this.animateText = this.animateText.bind(this);
         this.state = {
+            active: false,
             inView: false,
-            animate: false
+            animate: true,
+            title: this.props.title
         };
     }
 
-    handleScroll = () => {
-        const top = this.sectionTitle.getBoundingClientRect().top;
-        if ((top) >= 0 && (top) <= window.innerHeight) {
-            window.removeEventListener('scroll', this.handleScroll);
-            this.setState({
-                animate: true,
-                inView: true
-            });
-        }
-    };
+    // handleScroll = () => {
+    //     const top = this.sectionTitle.getBoundingClientRect().top;
+    //     if ((top) >= 0 && (top) <= window.innerHeight) {
+    //         window.removeEventListener('scroll', this.handleScroll);
+    //         this.setState({
+    //             animate: true,
+    //             inView: true
+    //         });
+    //     }
+    // };
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        if (this.state.animate) {
-            this.animateText(this.sectionTitle.innerHTML, '', 100)
+        if (this.state.animate && this.state.inView && !this.state.active) {
+            this.animateText(this.state.title, '', 100)
         }
     }
 
     animateText(text, current, interval) {
-        if (text.length > 0 && this.sectionTitle) {
+        if (text.length > 0 && this.state.title) {
             current += text.substr(0, 1);
-            // console.log(this.sectionTitle);
-            this.sectionTitle.innerHTML = current;
+            this.setState({title: current, active: true});
             setTimeout(() => {
                 this.animateText(text.substr(1), current, interval);
             }, interval);
         } else {
-            this.setState({animate: false});
+            this.setState({animate: false, active: false});
         }
     }
 
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll);
-        this.handleScroll();
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.handleScroll)
-    }
-
     render() {
-        const {children, id, extraClass = "section", title, description} = this.props;
+        const {children, id, extraClass = "section", description} = this.props;
 
         return (
             <>
             <Container id={id} tag="section" className={extraClass}>
                 <Row>
                     <Col md="10">
-                        <h2 className="section__title" ref={node => this.sectionTitle = node}>{title}</h2>
+                        <InView onChange={(inView) => this.setState({inView: inView})} triggerOnce>
+                            {({inView, ref}) => (
+                                <h2 className="section__title" ref={ref}>{this.state.title}</h2>
+                            )}
+                        </InView>
                         {description ? <p className="section__description">{description}</p> : ''}
                     </Col>
                 </Row>
