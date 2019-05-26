0c7e16afc71d5d9f349f85b8d3c856a1fd605884
Vpsnak <info.vpsnak@gmail.com>
2019-05-26 12:44:26 +0300

    added polyfill for IntersectionObserver

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,61 +1,67 @@
-require("dotenv").config({
-    path: `.env.${process.env.NODE_ENV}`,
-});
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 
 module.exports = {
-    siteMetadata: {
-        title: `Evangelos Pallis`,
-        description: `Personal site`,
-        subtitle: `Custom Desc`,
-        author: `@vpsnak`,
+  siteMetadata: {
+    title: `Evangelos Pallis`,
+    description: `Personal site`,
+    subtitle: `Custom Desc`,
+    author: `@vpsnak`
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        baseUrl: 'vpsnak.com',
+        protocol: 'https',
+        hostingWPCOM: false,
+        useACF: true,
+        auth: {
+          jwt_user: process.env.JWT_USER,
+          jwt_pass: process.env.JWT_PASSWORD
+        },
+        verboseOutput: true,
+        perPage: 100,
+        includedRoutes: [
+          '**/categories',
+          '**/posts',
+          '**/pages',
+          '**/media',
+          '**/taxonomies',
+          '**/educations',
+          '**/employments',
+          '**/projects',
+          // "**/spirit/**",
+          // "**/spirit/v2/options",
+          // "**/settings",
+          '**/menus'
+        ]
+      }
     },
-    plugins: [
-        {
-            resolve: "gatsby-source-wordpress",
-            options: {
-                baseUrl: "vpsnak.com",
-                protocol: "https",
-                hostingWPCOM: false,
-                useACF: true,
-                auth: {
-                    jwt_user: process.env.JWT_USER,
-                    jwt_pass: process.env.JWT_PASSWORD,
-                },
-                verboseOutput: true,
-                perPage: 100,
-                includedRoutes: [
-                    "**/categories",
-                    "**/posts",
-                    "**/pages",
-                    "**/media",
-                    "**/taxonomies",
-                    "**/educations",
-                    "**/employments",
-                    "**/projects",
-                    // "**/spirit/**",
-                    // "**/spirit/v2/options",
-                    // "**/settings",
-                    "**/menus",
-                ],
-            },
-        },
-        `gatsby-plugin-emotion`,
-        `gatsby-transformer-sharp`,
-        {
-            resolve: `gatsby-plugin-sharp`,
-            options: {
-                useMozJpeg: false,
-                stripMetadata: true,
-                defaultQuality: 75,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/src/assets/`,
-            }
-        },
-        'gatsby-plugin-react-helmet',
-        // 'gatsby-plugin-netlify',
-    ],
-};
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-polyfill-io`,
+      options: {
+        features: [`IntersectionObserver`]
+      }
+    },
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        useMozJpeg: false,
+        stripMetadata: true,
+        defaultQuality: 75
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/assets/`
+      }
+    },
+    'gatsby-plugin-react-helmet'
+    // 'gatsby-plugin-netlify',
+  ]
+}
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,10 @@
-const path = require(`path`);
-const slash = require(`slash`);
+const path = require(`path`)
+const slash = require(`slash`)
 
 exports.createPages = async ({graphql, actions}) => {
-    const {createPage} = actions;
+  const {createPage} = actions
 
-    const result = await graphql(`
+  const result = await graphql(`
     {
       allWordpressPage {
         edges {
@@ -28,36 +28,36 @@ exports.createPages = async ({graphql, actions}) => {
         }
       }
     }
-  `);
+  `)
 
-    if (result.errors) {
-        throw new Error(result.errors)
-    }
+  if (result.errors) {
+    throw new Error(result.errors)
+  }
 
-    const {
-        allWordpressPage,
-        allWordpressPost
-    } = result.data;
+  const {
+    allWordpressPage,
+    allWordpressPost
+  } = result.data
 
-    const pageTemplate = path.resolve(`./src/templates/page.js`);
-    allWordpressPage.edges.forEach(edge => {
-        createPage({
-            path: `/${edge.node.slug}/`,
-            component: slash(pageTemplate),
-            context: {
-                id: edge.node.id,
-            },
-        })
-    });
-
-    const postTemplate = path.resolve(`./src/templates/post.js`);
-    allWordpressPost.edges.forEach(edge => {
-        createPage({
-            path: `/${edge.node.slug}/`,
-            component: slash(postTemplate),
-            context: {
-                id: edge.node.id,
-            },
-        })
+  const pageTemplate = path.resolve(`./src/templates/page.js`)
+  allWordpressPage.edges.forEach(edge => {
+    createPage({
+      path: `/${edge.node.slug}/`,
+      component: slash(pageTemplate),
+      context: {
+        id: edge.node.id
+      }
     })
-};
+  })
+
+  const postTemplate = path.resolve(`./src/templates/post.js`)
+  allWordpressPost.edges.forEach(edge => {
+    createPage({
+      path: `/${edge.node.slug}/`,
+      component: slash(postTemplate),
+      context: {
+        id: edge.node.id
+      }
+    })
+  })
+}
