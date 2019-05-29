7267ec93a973d1bf07be49128d9bd2e725de8f36
Vpsnak <info.vpsnak@gmail.com>
2019-05-29 22:30:27 +0300

    added hotjar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import { hotjar } from 'react-hotjar';
 import Header from './Header'
 import Footer from './Footer'
 import '../../static/css/main.css'
@@ -9,6 +9,9 @@ const Layout = ({children, ...props}) => {
   const {
     isHome = false
   } = props
+
+  hotjar.initialize(1342300, 6);
+
   return (
     <div className={`d-flex flex-column`}>
       <Header isHome={isHome} />
